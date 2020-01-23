63de8b42ee73fe0a56dccc4be63aa75da5ccdd3d
jaikumarsevarkodiyon <60220912+jaikumarsevarkodiyon@users.noreply.github.com>
2020-01-23 18:08:52 +0530

    Create Add.py

---
+++ Add.py
@@ -0,0 +1,16 @@
+a=int(input("enter the input"))
+b=int(input("enter the input"))
+c=a+b
+print(c)
+
+
+def solveMeFirst(a,b):
+    # Hint: Type return a+b below
+    res=a+b
+    return res
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
